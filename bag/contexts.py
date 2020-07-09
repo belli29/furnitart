5ba53822f3941a856fffc78a0244d8acc2076fa0
belli29 <belli29@hotmail.com>
2020-07-09 14:44:49 +0000

    added add_to_bag view and developed bag.html

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
@@ -7,6 +9,19 @@ def bag_contents(request):
     total = 0
     product_count = 0
     irl_delivery = False
+    bag = request.session.get('bag', {})
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        subtotal = quantity * product.price
+        total += subtotal
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+            'total':subtotal,
+        })
+
 
     def delivery_calculation (free_delivery_treshold, standard_delivery_percentage):
         if total < free_delivery_treshold:
@@ -32,6 +47,7 @@ def bag_contents(request):
     free_delivery_delta = results['free_delivery_delta']
     free_delivery_treshold = results['free_delivery_threshold']   
     grand_total = delivery + total
+    
     
     context = {
         'bag_items': bag_items,
