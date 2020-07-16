9d8ef64f7be666271868a12f2114ef80f45215f1
belli29 <belli29@hotmail.com>
2020-07-16 22:47:44 +0000

    improved toast structure

--- bag/views.py
+++ bag/views.py
@@ -16,39 +16,32 @@ def add_to_bag(request, item_id):
     bag = request.session.get('bag', {})
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
-
     request.session['bag'] = bag
     return redirect(redirect_url)
 
-def remove_from_bag(request, item_id):
-    """ Remove a specific product from the shopping bag """
-
-    bag = request.session.get('bag', {})
-    try:
-        del bag[item_id]
-        request.session['bag'] = bag
-    except KeyError as ex:
-        print("No such key: '%s'" % ex.message)
-    return redirect("view_bag")
-
 def update_bag(request, item_id):
     """ amend the amount of a specific item in the bag"""
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
     bag[item_id] = quantity
     request.session['bag'] = bag
+    messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     return redirect("view_bag")
 
 def remove_from_bag(request, item_id):
     """ Remove a specific product from the shopping bag """
-
+    product = get_object_or_404(Product, pk=item_id)
     bag = request.session.get('bag', {})
     try:
         del bag[item_id]
         request.session['bag'] = bag
+        messages.success(request, f'Removed {product.name} from the bag')
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing item:{e}')
         return HttpResponse(status=500)
