cde5bf958898e7ed3993cb5c0e4cf2249f81b246
belli29 <belli29@hotmail.com>
2020-09-12 08:52:38 +0000

    added models test for checkout

--- checkout/test_models.py
+++ checkout/test_models.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
-from .models import Order, OrderLineItem
+from decimal import *
+from .models import Order, OrderLineItem, PreOrder, PreOrderLineItem
+from products.models import Product
 
 class TestProductModel(TestCase):
 
-    def test_shipped_defaults_to_True(self):
+    def test_shipped_defaults_to_True(self): 
         order = Order(
                 full_name="test",
                 email="test",
@@ -14,8 +16,84 @@ class TestProductModel(TestCase):
                 street_address1="test",
                 street_address2="test",
                 county="test",
-                order_total=10,
                 original_bag="test"            
         )
         order.save()
         self.assertFalse(order.shipped)  
+
+    def test_order_total(self):
+        #delivery to Ireland
+        product = Product(name="product", price=10)
+        product2 = Product(name="product2", price=5) 
+        product.save()
+        product2.save()
+        order = Order(
+                full_name="test",
+                email="test",
+                phone_number="12",
+                country="IE",
+                postcode="test",
+                town_or_city="test",
+                street_address1="test",
+                street_address2="test",
+                county="test",
+                original_bag="test"            
+        )
+        order.save()
+        orderlineitem = OrderLineItem(
+            order=order,
+            product=product,
+            quantity=2,
+        )
+        orderlineitem2 = OrderLineItem(
+            order=order,
+            product=product2,
+            quantity=2,
+        )
+        orderlineitem.save()
+        orderlineitem2.save()
+        self.assertEqual(order.grand_total, 33)
+        #same, order, delivery to EU
+        order.country = "DE"
+        order.save()
+        orderlineitem.save()
+        orderlineitem2.save()
+        self.assertEqual(order.grand_total, 36)
+
+    def test_preorder_total(self):
+        #delivery to Ireland
+        product = Product(name="product", price=10)
+        product2 = Product(name="product2", price=5) 
+        product.save()
+        product2.save()
+        order = PreOrder(
+                full_name="test",
+                email="test",
+                phone_number="12",
+                country="IE",
+                postcode="test",
+                town_or_city="test",
+                street_address1="test",
+                street_address2="test",
+                county="test",           
+        )
+        order.save()
+        orderlineitem = PreOrderLineItem(
+            order=order,
+            product=product,
+            quantity=2,
+        )
+        orderlineitem2 = PreOrderLineItem(
+            order=order,
+            product=product2,
+            quantity=2,
+        )
+        orderlineitem.save()
+        orderlineitem2.save()
+        self.assertEqual(order.grand_total, Decimal('31.35') )
+        #same, order, delivery to EU
+        order.country = "DE"
+        order.save()
+        orderlineitem.save()
+        orderlineitem2.save()
+        self.assertEqual(order.grand_total, Decimal('34.20')) 
