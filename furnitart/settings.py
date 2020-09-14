c45127d18f0d8ffb5d646bf7f850d81b659cedf9
belli29 <belli29@hotmail.com>
2020-09-14 22:20:51 +0000

    corrected allowed host

--- furnitart/settings.py
+++ furnitart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '.gitpod.io', '127.0.0.1', '0.0.0.0', 'furnitart.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', '.gitpod.io', '127.0.0.1', '0.0.0.0', 'furnitart.herokuapp.com']
 
 
 # Application definition
