c53c458ae77ac091fad46d87b607d8d744684479
belli29 <belli29@hotmail.com>
2020-09-15 22:49:01 +0000

    added smtp server settings to deliver email

--- furnitart/settings.py
+++ furnitart/settings.py
@@ -104,7 +104,6 @@ AUTHENTICATION_BACKENDS = [
 
 # allauth settings
 SITE_ID = 1
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 CCOUNT_EMAIL_VERIFICATION = 'required'
@@ -223,4 +222,14 @@ COUNTRIES_ONLY = ['AT', 'BE', 'HR', 'BG', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'D
 
 # Email
 
-DEFAULT_FROM_EMAIL = 'furnitart@example.com'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'furnitart@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
