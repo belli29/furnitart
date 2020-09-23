e5f0645ec62999448b2a49b86d2bb8547561ccf3
belli29 <belli29@hotmail.com>
2020-09-23 16:34:03 +0000

    amended settings file

--- furnitart/settings.py
+++ furnitart/settings.py
@@ -231,3 +231,4 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    IT_SUPPORT_EMAIL = os.environ.get('IT_SUPPORT_EMAIL')
