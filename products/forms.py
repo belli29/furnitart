561694dd507292d0c0302ef9a60685c68e2f29e5
belli29 <belli29@hotmail.com>
2020-09-28 19:56:15 +0000

    installed django cleanup

--- products/forms.py
+++ products/forms.py
@@ -12,7 +12,8 @@ class ProductForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+        self.fields['image'].label = "Image" \
+            " (please use pictures 500px*500px for better results)"
